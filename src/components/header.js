d7c1cf1ebd659d16548d7b82f0757c2cb0c1cbd6
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2020-09-07 19:39:20 +0200

    added config for netlify

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(0, 204, 204)`,
       marginBottom: `1.45rem`,
     }}
   >
